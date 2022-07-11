46bb7ee6b21138532fc43e0eade6b209789370a3
Glen Ottley <glenottley@gmail.com>
2022-07-11 19:26:56 -0400

    updated page title, description, favicon, fixed header image issue, fixed progress bar overflow issue

--- src/App.js
+++ src/App.js
@@ -1,123 +1,115 @@
-import React, { useState, useEffect } from "react";
-import productData from "./data.js";
-import Header from "./components/Header";
-import Menu from "./components/Menu";
-import ActionCall from "./components/ActionCall";
-import PledgeStats from "./components/PledgeStats";
-import NavModal from "./components/modals/NavModal";
-import PledgeModal from "./components/modals/PledgeModal";
-import ThanksModal from "./components/modals/ThanksModal";
-import About from "./components/About";
-
+import React, { useState, useEffect } from 'react'
+import productData from './data.js'
+import Menu from './components/Menu'
+import ActionCall from './components/ActionCall'
+import PledgeStats from './components/PledgeStats'
+import NavModal from './components/modals/NavModal'
+import PledgeModal from './components/modals/PledgeModal'
+import ThanksModal from './components/modals/ThanksModal'
+import About from './components/About'
 
 function App() {
-	const [stats, setStats] = useState({
-		amount: 89914,
-		goal: 100000,
-		backers: 5007,
-		daysRemaining: 56 
-	})
+  const [stats, setStats] = useState({
+    amount: 89914,
+    goal: 100000,
+    backers: 5007,
+    daysRemaining: 56,
+  })
 
-	const [products, setProducts] = useState([]);
-	const [showNavModal, setShowNavModal] = useState(false);
-	const [showPledgeModal, setShowPledgeModal] = useState(false);
-	const [showThanksModal, setShowThanksModal] = useState(false);
+  const [products, setProducts] = useState([])
+  const [showNavModal, setShowNavModal] = useState(false)
+  const [showPledgeModal, setShowPledgeModal] = useState(false)
+  const [showThanksModal, setShowThanksModal] = useState(false)
 
-	useEffect(() => {
-		setProducts(productData)
-	}, [products])
+  useEffect(() => {
+    setProducts(productData)
+  }, [products])
 
-	const toggleNavModal = () => {
-		showNavModal
-		? setShowNavModal(false)
-		: setShowNavModal(true)
-	}
+  const toggleNavModal = () => {
+    showNavModal ? setShowNavModal(false) : setShowNavModal(true)
+  }
 
-	const togglePledgeModal = (e) => {
-		showPledgeModal 
-		? setShowPledgeModal(false) 
-		: setShowPledgeModal(true)
-		if (e.target.id) { 
-			selectProduct(e)
-		}
-	}
+  const togglePledgeModal = (e) => {
+    showPledgeModal ? setShowPledgeModal(false) : setShowPledgeModal(true)
+    if (e.target.id) {
+      selectProduct(e)
+    }
+  }
 
-	const toggleThanksModal = () => {
-		showThanksModal 
-		? setShowThanksModal(false) 
-		: setShowThanksModal(true)
-	}
+  const toggleThanksModal = () => {
+    showThanksModal ? setShowThanksModal(false) : setShowThanksModal(true)
+  }
 
-	const selectProduct = ({ target }) => {
-		let index = products.findIndex(product => product.id === target.id)
-		let newProducts = [...products];
-		newProducts[index] = {...products[index], ...products[index].selected = true}
+  const selectProduct = ({ target }) => {
+    let index = products.findIndex((product) => product.id === target.id)
+    let newProducts = [...products]
+    newProducts[index] = {
+      ...products[index],
+      ...(products[index].selected = true),
+    }
 
-		for (let i = 0; i < newProducts.length; i++) {
-			if (i !== index)  {
-				newProducts[i].selected = false
-			}
-		}	
-		setProducts(newProducts)
-	}
+    for (let i = 0; i < newProducts.length; i++) {
+      if (i !== index) {
+        newProducts[i].selected = false
+      }
+    }
+    setProducts(newProducts)
+  }
 
-	const submitPledge = ({ target }) => {
-		let pledgeAmount = Number(target.parentNode.childNodes[0].value)
-		stats.amount += pledgeAmount
-		stats.backers ++
-		setStats(prevStats => ({...prevStats, stats }));
+  const submitPledge = ({ target }) => {
+    let pledgeAmount = Number(target.parentNode.childNodes[0].value)
+    stats.amount += pledgeAmount
+    stats.backers++
+    setStats((prevStats) => ({ ...prevStats, stats }))
 
-		let index = productData.findIndex(product => product.id === target.id)
-		productData[index].remaining --
-		products[index].selected = false
-		setShowPledgeModal(false)
-		toggleThanksModal()
-	}
+    let index = productData.findIndex((product) => product.id === target.id)
+    productData[index].remaining--
+    products[index].selected = false
+    setShowPledgeModal(false)
+    toggleThanksModal()
+  }
 
-	const scrollToProduct = (selected, id) => {
-		if (selected) {
-			document.getElementById(id).scrollIntoView()
-		}
-	}
+  const scrollToProduct = (selected, id) => {
+    if (selected) {
+      document.getElementById(id).scrollIntoView()
+    }
+  }
 
   return (
-    <div className="App">
-    	<Header />
+    <div className='App'>
+      <div className='header'></div>
 
-    	<NavModal 
-    	showNavModal={showNavModal}
-    	toggleNavModal={toggleNavModal} />
+      <NavModal showNavModal={showNavModal} toggleNavModal={toggleNavModal} />
 
-    	<PledgeModal 
-    	showPledgeModal={showPledgeModal}
-    	togglePledgeModal={togglePledgeModal}
-    	products={products} 
-    	selectProduct={selectProduct} 
-    	submitPledge={submitPledge}
-    	scrollToProduct={scrollToProduct} />
+      <PledgeModal
+        showPledgeModal={showPledgeModal}
+        togglePledgeModal={togglePledgeModal}
+        products={products}
+        selectProduct={selectProduct}
+        submitPledge={submitPledge}
+        scrollToProduct={scrollToProduct}
+      />
 
-    	<ThanksModal
-    	showThanksModal={showThanksModal}
-    	toggleThanksModal={toggleThanksModal} />
+      <ThanksModal
+        showThanksModal={showThanksModal}
+        toggleThanksModal={toggleThanksModal}
+      />
 
-    	<Menu 
-	    	showNav={showNavModal}
-	    	toggleNavModal={toggleNavModal} />
+      <Menu showNav={showNavModal} toggleNavModal={toggleNavModal} />
 
-    	<main className="main container column">
-	    	
+      <main className='main container column'>
+        <ActionCall togglePledgeModal={togglePledgeModal} />
 
-	    	<ActionCall togglePledgeModal={togglePledgeModal} />
+        <PledgeStats stats={stats} />
 
-	    	<PledgeStats stats={stats} />
-
-	    	<About 
-	    	products={products} 
-	    	togglePledgeModal={togglePledgeModal}
-	    	selectProduct={selectProduct} />
-    	</main>
+        <About
+          products={products}
+          togglePledgeModal={togglePledgeModal}
+          selectProduct={selectProduct}
+        />
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
